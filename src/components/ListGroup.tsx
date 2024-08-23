4b70d38c2059c5e1548c16c38751812c09a084c7
Steven Vore <steven.vore@coxautoinc.com>
2024-08-23 10:09:21 -0400

    0.02 simple conditional

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,17 +1,29 @@
-import { Fragment } from "react";
-
 function ListGroup() {
+  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  items = [];
+
+  // if (items.length === 0)
+  //   return (
+  //     <>
+  //       <h1>Cities</h1>
+  //       <p>No items found.</p>
+  //     </>
+  //   );
+
+  const getMessage = () => {
+    return items.length === 0 ? <p>No items found.</p> : null;
+  };
+
   return (
-    <Fragment>
-      <h1>List</h1>
+    <>
+      <h1>Cities</h1>
+      {getMessage()}
       <ul className="list-group">
-        <li className="list-group-item">An item</li>
-        <li className="list-group-item">A second item</li>
-        <li className="list-group-item">A third item</li>
-        <li className="list-group-item">A fourth item</li>
-        <li className="list-group-item">And a fifth one</li>
+        {items.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
       </ul>
-    </Fragment>
+    </>
   );
 }
 
